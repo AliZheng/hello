8d70b5d5feb1b02ee6bb690ef3acacae6b4dc991
AliZheng <aliama0288@gmail.com>
2015-01-22 10:27:09 +0800

    somechanges

--- CItest.java
+++ CItest.java
@@ -1,10 +1,12 @@
+
 public class CItest {
 	static int result(int i)
 	{
 		return i+0;
-		
+	
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//weeeeee;
 System.out.println("Hello    worlds");}
 }
